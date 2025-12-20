b13e20987dd868d8c4ef43e3babc3890ccefabbb
Mathis Makarski <mathis.makarski@menschcologne.com>
2025-12-20 14:03:30 +0100

    add arial narrow font

---
+++ nextjs-kuenstlerei/tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-arial-narrow)", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
